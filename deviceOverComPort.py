7c325d5b89475093a2ffe3c2686ee23907c63078
ProgrammingMind <atul6101989@gmail.com>
2020-05-13 16:37:53 +0530

    Update deviceOverComPort.py

--- deviceOverComPort.py
+++ deviceOverComPort.py
@@ -1,34 +1,40 @@
 from serial.tools.list_ports import comports
 import serial
 
+
+#   Get ALL the port
 def get_ports():
-    ports = comports()
-    return ports
-    print(ports[0])
-
-def finddevice(portsFound):
-    commPort = 'None'
-    numConnection = len(portsFound)
-
-    for i in range(0,numConnection):
-        port = foundPorts[i]
-        strPort = str(port)
-
-        if 'PI USB to Serial' in strPort:
-            splitPort = strPort.split(' ')
-            commport   = (splitPort[0])
-
-    return commport
+    total_ports = comports()
+    return total_ports
+    # print(all_ports[0])
 
 
-if __name__ == '__main__':
-    foundPorts = get_ports()
-    connectPort = finddevice(foundPorts)
+def find_port():
+    comm_port = 'None'
+    all_ports = get_ports()
+    num_connection = len(all_ports)
+    try:
+        for i in range(0, num_connection):
+            port = all_ports[i]
+            str_port = str(port)
 
-    if connectPort != 'None':
-        ser = serial.Serial(connectPort, baudrate=9600, timeout=1)
-        print('Connected to ' +connectPort)
-    else:
-        print('Connection Issue!')
+            if 'PI USB to Serial' in str_port:
+                splitport = str_port.split(' ')
+                comm_port = (splitport[0])
 
-    print('DONE')
+        return comm_port
+    except:
+        print("USB is not connected properly with Updater, "
+              "Please connect and retry")
+
+#
+# if __name__ == '__main__':
+#     connectPort = find_port()
+#     if connectPort != 'None':
+#         ser = serial.Serial(connectPort, baudrate=9600, timeout=1)
+#         print('Connected to ' + connectPort)
+#
+#     else:
+#         print('Connection Issue!')
+#
+#     print('DONE')
