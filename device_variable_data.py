45e524c6569d0e15b59c9aaf568c2205721f5a62
ProgrammingMind <atul6101989@gmail.com>
2020-05-16 11:48:25 +0530

    Update and rename updater_data.py to device_variable_data.py

---
+++ device_variable_data.py
@@ -0,0 +1,8 @@
+def get_variables(commands):
+    Operations = {
+                "device_VERSION": "adriuno_product_version.sh \r\n",
+                "device_STATUS": "config_status_adurino.sh \r\n",
+                "reset": "uconfig -r",
+                "deviceReboot":"reboot"
+                 }
+  
